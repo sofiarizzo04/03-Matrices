8cc4d699e3e177bacfaa6978917c726123e969a9
sofiarizzo04 <sofirizzo412@gmail.com>
2026-06-26 20:27:01 -0300

    resolvi12

--- homework3.js
+++ homework3.js
@@ -200,12 +200,34 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let mesesBuscados = ["Enero", "Marzo", "Noviembre"];
+  let resultado = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      resultado.push(array[i]);
+    }
+  }
+
+  if (resultado.length === 3) {
+    return resultado;
+  } else {
+    return "No se encontraron los meses pedidos";
+  }
+
 }
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let resultado = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 100) {
+      resultado.push(array[i]);
+    }
+  }
+  return resultado;
 }
 
 function breakStatement(numero) {
