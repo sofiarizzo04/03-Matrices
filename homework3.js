3b6b5749aed7f8e9f10021b3248deb20566a6d71
sofiarizzo04 <sofirizzo412@gmail.com>
2026-06-26 19:28:40 -0300

    resolvi8

--- homework3.js
+++ homework3.js
@@ -132,7 +132,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo) {
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  
+  let contador = 0;
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 18) {
+      contador++;
+    }
+  }
+  return contador;
 }
 
 function diaDeLaSemana(numeroDeDia) {
