1a8b8706df395f83e1dfdbc8665551a2fc54d6d2
sofiarizzo04 <sofirizzo412@gmail.com>
2026-06-26 18:15:40 -0300

    resolvi2

--- homework3.js
+++ homework3.js
@@ -31,6 +31,9 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  return array.map(function(num) {
+    return num + 1;
+  });
   
 }
 
@@ -38,6 +41,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
